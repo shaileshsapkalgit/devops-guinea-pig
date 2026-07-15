35610c3e35bfc15f26fb0da44cafc3440d1f6816
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2026-07-15 22:47:01 +0530

    change to pig

--- src/main/java/com/shailesh/devops_guinea_pig/controller/HealthController.java
+++ src/main/java/com/shailesh/devops_guinea_pig/controller/HealthController.java
@@ -1,11 +1,13 @@
 package com.shailesh.devops_guinea_pig.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/pig")
 public class HealthController {
     @GetMapping("/health")
     public Map<String,String> health(){
