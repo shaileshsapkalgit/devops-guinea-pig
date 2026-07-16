5042ee994cb9bbbe7a70222dbd849ff409d65e50
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2026-07-16 13:01:57 +0530

    Remove /pig prefix, revert to original endpoints

--- src/main/java/com/shailesh/devops_guinea_pig/controller/HealthController.java
+++ src/main/java/com/shailesh/devops_guinea_pig/controller/HealthController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/pig")
 public class HealthController {
     @GetMapping("/health")
     public Map<String,String> health(){
