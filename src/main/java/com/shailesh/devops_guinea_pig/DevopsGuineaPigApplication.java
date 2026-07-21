dbaae65c25a5b16cc62f054863d0e107ac88176f
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2026-07-21 23:26:31 +0530

    Add README to trigger pipeline

--- src/main/java/com/shailesh/devops_guinea_pig/DevopsGuineaPigApplication.java
+++ src/main/java/com/shailesh/devops_guinea_pig/DevopsGuineaPigApplication.java
@@ -8,7 +8,7 @@ public class DevopsGuineaPigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsGuineaPigApplication.class, args);
-		System.out.println("Hello world!!!");
+		System.out.println("Hello world!! !");
 	}
 
 }
