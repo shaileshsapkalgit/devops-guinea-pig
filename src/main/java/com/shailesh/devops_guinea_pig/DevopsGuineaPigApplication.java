3b1d8bdad2b2a5ca9bf004b40cf52cea010ba07a
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2026-07-22 01:58:42 +0530

    docker  added

--- src/main/java/com/shailesh/devops_guinea_pig/DevopsGuineaPigApplication.java
+++ src/main/java/com/shailesh/devops_guinea_pig/DevopsGuineaPigApplication.java
@@ -8,6 +8,6 @@ public class DevopsGuineaPigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsGuineaPigApplication.class, args);
-		System.out.println("Hello world!! !");
+		System.out.println("Hello world");
 	}
 }
